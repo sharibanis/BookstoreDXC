50912897af427f4a1052304028ba65b117b90750
sharibanis <sharib.anis@live.com>
2026-06-07 23:29:09 +0800

    updated tests

--- src/test/java/bookstore/sharib/ServiceControllerTest.java
+++ src/test/java/bookstore/sharib/ServiceControllerTest.java
@@ -11,19 +11,23 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-import static io.restassured.RestAssured.given;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 
-import bookstore.sharib.BookstoreApplication;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.json.JSONArray;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
 	classes = BookstoreApplication.class
 )
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ServiceControllerTest {
 	private static final Logger log = LoggerFactory.getLogger(ServiceControllerTest.class);
 	private static final String baseUri = "http://localhost:8080/"; 
-	private final String PASS = ""; 
     
 	@BeforeAll
 	public static void setUp() throws Exception {
@@ -32,56 +36,51 @@ public class ServiceControllerTest {
 	}
 	
 	@Test
+	@Order(1)
 	public void whenRequestHealth_thenOK() {
     	log.info("whenRequestHealth_thenOK()");
-		given().log().uri().when().get("/actuator/health");
+		//given().log().uri().when().get("/actuator/health");
 		Response response = RestAssured.get("/actuator/health");
 		response.then().assertThat().statusCode(200).log().all();
 	}
 
 	@Test
+	@Order(2)
 	public void whenRequestGetFind_thenOK() {
     	log.info("whenRequestGetFind_thenOK()");
-		//RequestSpecification request = RestAssured.given();
-		/*request.header("Content-Type", "application/json");
-		request.auth().basic("user", "password")
-				.when().get("/bookstore/public/find?title=SomeTitle&author=SomeAuthor")
-				.then().assertThat().statusCode(200).log().all();*/
-		//given().log().uri().when().get("/bookstore/public/find");
-		//Response response = RestAssured.get("/bookstore/public/find?title=SomeTitle&author=SomeAuthor");
-		//response.then().assertThat().statusCode(200).log().all();
-		//response.then().assertThat().body("some.property", equalTo("expected value"));
-		given()
-		.auth().basic("user", "password")
-		.when()
-		.get("/bookstore/public/find?title=SomeTitle&author=SomeAuthor")
-		.then()
-		.statusCode(200)
-		.log().all();
+		Response response = RestAssured.get("/bookstore/public/find?title=1984&author=George%20Orwell");
+		response.then().assertThat().statusCode(200).log().all();
+		response.then().assertThat().body("title", equalTo("1984"));
 	}
 	
-	//@Test
+	@Test
+	@Order(3)
 	public void whenRequestGetFind_thenNotOK() {
     	log.info("whenRequestGetFind_thenNotOK()");
+		Response response = RestAssured.get("/bookstore/public/find?title=1994&author=George%20Orwell");
+		response.then().assertThat().statusCode(401).log().all();
 	}
 	
-	//@Test
-	public void whenRequestAddBook_thenOK() {
-    	log.info("whenRequestAddBook_thenOK()");
-		/*try {
+	@Test
+	@Order(4)
+	public void whenRequestPostAddBook_thenOK() {
+    	log.info("whenRequestPostAddBook_thenOK()");
+		try {
 			JSONObject requestBody = new JSONObject(); 
-			requestBody.put("ISBN", 1234567890123L);
+			requestBody.put("isbn", "1234567891123");
 			requestBody.put("title", "Test Book");
-			requestBody.put("authors", "Test Author");
+			JSONArray authorArray = new JSONArray();
+			authorArray.put("Test Author");
+			requestBody.put("authors", authorArray);
 			requestBody.put("yearPublished", 2024);
 			requestBody.put("price", 19.99);
 			requestBody.put("genre", "Test Genre");
+			log.info("requestBody: {}", requestBody.toString());
 			RequestSpecification request = RestAssured.given();
 			request.header("Content-Type", "application/json");
 			request.body(requestBody.toString()); 
-			request.auth()
-			.basic("user", "password")
-			.when()
+			request.auth().basic("user", "password");
+			request.when()
 			.post("/bookstore/addBook")
 			.then()
 			.assertThat()
@@ -89,10 +88,64 @@ public class ServiceControllerTest {
 			log.info("Book {} added successfully.", "Test Book");
 		} catch (Exception ex) {
 			log.warn("Exception occurred while adding book {}. ", 
-				"Test Book", ex.toString());
-		}*/
+				"Test Book");
+			log.warn("Exception: ", ex);
+		}
 	}
 	
+	@Test
+	@Order(5)
+	public void whenRequestPutUpdateBook_thenOK() {
+    	log.info("whenRequestPutUpdateBook_thenOK()");
+		try {
+			JSONObject requestBody = new JSONObject(); 
+			requestBody.put("isbn", "1234567891123");
+			requestBody.put("title", "Test Book");
+			JSONArray authorArray = new JSONArray();
+			authorArray.put("Test Author");
+			requestBody.put("authors", authorArray);
+			requestBody.put("yearPublished", 2024);
+			requestBody.put("price", 19.99);
+			requestBody.put("genre", "Test Genre");
+			log.info("requestBody: {}", requestBody.toString());
+			RequestSpecification request = RestAssured.given();
+			request.header("Content-Type", "application/json");
+			request.body(requestBody.toString()); 
+			request.auth().basic("user", "password");
+			request.when()
+			.put("/bookstore/1234567891123")
+			.then()
+			.assertThat()
+			.statusCode(HttpStatus.OK.value());
+			log.info("Book {} updated successfully.", "Test Book");
+		} catch (Exception ex) {
+			log.warn("Exception occurred while updating book {}. ", 
+				"Test Book");
+			log.warn("Exception: ", ex);
+		}
+	}
+
+	@Test
+	@Order(6)
+	public void whenRequestDeleteDeleteBook_thenOK() {
+    	log.info("whenRequestDeleteDeleteBook_thenOK()");
+		try {
+			RequestSpecification request = RestAssured.given();
+			request.header("Content-Type", "application/json");
+			request.auth().basic("user", "password");
+			request.when()
+			.delete("/bookstore/1234567891123")
+			.then()
+			.assertThat()
+			.statusCode(HttpStatus.OK.value());
+			log.info("Book {} deleted successfully.", "Test Book");
+		} catch (Exception ex) {
+			log.warn("Exception occurred while deleting book {}. ", 
+				"Test Book");
+			log.warn("Exception: ", ex);
+		}
+	}
+
 	@AfterAll
 	public static void cleanUp(){
 		log.info("@AfterAll cleanUp() method called");
